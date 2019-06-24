1eeb7466d2d8686cc828af895731f49457a81341
Wolf <Wolf@DESKTOP-21SVAEG>
2019-06-24 11:50:52 +0900

    .

---
+++ quizstart/quizstart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quizstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heldasdlo  Wortdld!");
+        }
+    }
+}
